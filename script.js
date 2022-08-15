7b2d41f4284b6be0a5ffb44665efd5b8e56df4e3
Lucas Oliveira <olivmath@protonmail.com>
2022-08-14 22:10:51 -0300

    update: refactor clean code

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
+const progressContainer = document.querySelector('.progress-container')
 const musicContainer = document.querySelector('.music-container')
+const progress = document.querySelector('.progress')
+const currTime = document.querySelector('#currTime')
+const durTime = document.querySelector('#durTime')
 const playBtn = document.querySelector('#play')
 const prevBtn = document.querySelector('#prev')
 const nextBtn = document.querySelector('#next')
-
 const audio = document.querySelector('#audio')
-const progress = document.querySelector('.progress')
-const progressContainer = document.querySelector('.progress-container')
 const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
-const currTime = document.querySelector('#currTime')
-const durTime = document.querySelector('#durTime')
+
 
 // Song titles / Títulos
 
-const songs = ['Never Surrender - Anno Domini Beats', 'Sinister - Anno Domini Beats', 'Skylines - Anno Domini Beats', 'Thats What It Takes instrumental NEFFEX', 'The Itch Instrumental NEFFEX', 'Pray - Anno Domini Beats', 'Intellect - Yung Logos', 'Break Your Lock and Key - Mini Vandals', 'March On - Ethan Meixsell'] 
+const songs = [
+    "Break Your Lock and Key - Mini Vandals",
+    "Intellect - Yung Logos",
+    "March On - Ethan Meixsell",
+    "Never Surrender - Anno Domini Beats",
+    "Pray - Anno Domini Beats",
+    "Sinister - Anno Domini Beats",
+    "Skylines - Anno Domini Beats",
+    "Thats What It Takes instrumental NEFFEX",
+    "The Itch Instrumental NEFFEX",
+]
 
 
 let songIndex = 2
 
 // Initially load song info /Carrega as informações
-loadSong (songs[songIndex])
+loadSong(songs[songIndex])
 
 // Update song details /Atualizar detalhes da música
-function loadSong(song) 
-{
+function loadSong(song) {
     title.innerText = song
     audio.src = `music/${song}.mp3`
     cover.src = `images/${song}.jpg`
 }
+
 // Play song
 function playSong() {
     musicContainer.classList.add('play')
@@ -49,9 +59,9 @@ function pauseSong() {
 function prevSong() {
     songIndex--
 
-        if(songIndex < 0) {
-            songIndex = songs.length - 1
-        }
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
     loadSong(songs[songIndex])
 
     playSong()
@@ -61,18 +71,18 @@ function prevSong() {
 function nextSong() {
     songIndex++
 
-        if(songIndex > songs.length - 1) {
-              songIndex = 0
-        }
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
     loadSong(songs[songIndex])
 
     playSong()
-    
+
 }
 
 //Update progress bar / Barra de Duração
 function updateProgress(e) {
-    const {duration, currentTime} = e.srcElement
+    const { duration, currentTime } = e.srcElement
     const progressPercent = (currentTime / duration) * 100
     progress.style.width = `${progressPercent}%`
 }
@@ -86,65 +96,65 @@ function setProgress(e) {
     audio.currentTime = (clickX / width) * duration
 }
 
-function DurTime (e) {
-	const {duration,currentTime} = e.srcElement;
-	var sec;
-	var sec_d;
-
-	
-	let min = (currentTime==null)? 0:
-	 Math.floor(currentTime/60);
-	 min = min <10 ? '0'+min:min;
-
-	
-	function get_sec (x) {
-		if(Math.floor(x) >= 60){
-			
-			for (var i = 1; i<=60; i++){
-				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
-					sec = Math.floor(x) - (60*i);
-					sec = sec <10 ? '0'+sec:sec;
-				}
-			}
-		}else{
-		 	sec = Math.floor(x);
-		 	sec = sec <10 ? '0'+sec:sec;
-		 }
-	} 
-
-	get_sec (currentTime,sec);
-
-	
-	currTime.innerHTML = min +':'+ sec;
-
-	
-	let min_d = (isNaN(duration) === true)? '0':
-		Math.floor(duration/60);
-	 min_d = min_d <10 ? '0'+min_d:min_d;
+function DurTime(e) {
+    const { duration, currentTime } = e.srcElement;
+    var sec;
+    var sec_d;
 
 
-	 function get_sec_d (x) {
-		if(Math.floor(x) >= 60){
-			
-			for (var i = 1; i<=60; i++){
-				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
-					sec_d = Math.floor(x) - (60*i);
-					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
-				}
-			}
-		}else{
-		 	sec_d = (isNaN(duration) === true)? '0':
-		 	Math.floor(x);
-		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
-		 }
-	} 
+    let min = (currentTime == null) ? 0 :
+        Math.floor(currentTime / 60);
+    min = min < 10 ? '0' + min : min;
 
-	
-	get_sec_d (duration);
 
-	
-	durTime.innerHTML = min_d +':'+ sec_d;
-		
+    function get_sec(x) {
+        if (Math.floor(x) >= 60) {
+
+            for (var i = 1; i <= 60; i++) {
+                if (Math.floor(x) >= (60 * i) && Math.floor(x) < (60 * (i + 1))) {
+                    sec = Math.floor(x) - (60 * i);
+                    sec = sec < 10 ? '0' + sec : sec;
+                }
+            }
+        } else {
+            sec = Math.floor(x);
+            sec = sec < 10 ? '0' + sec : sec;
+        }
+    }
+
+    get_sec(currentTime, sec);
+
+
+    currTime.innerHTML = min + ':' + sec;
+
+
+    let min_d = (isNaN(duration) === true) ? '0' :
+        Math.floor(duration / 60);
+    min_d = min_d < 10 ? '0' + min_d : min_d;
+
+
+    function get_sec_d(x) {
+        if (Math.floor(x) >= 60) {
+
+            for (var i = 1; i <= 60; i++) {
+                if (Math.floor(x) >= (60 * i) && Math.floor(x) < (60 * (i + 1))) {
+                    sec_d = Math.floor(x) - (60 * i);
+                    sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
+                }
+            }
+        } else {
+            sec_d = (isNaN(duration) === true) ? '0' :
+                Math.floor(x);
+            sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
+        }
+    }
+
+
+    get_sec_d(duration);
+
+
+    durTime.innerHTML = min_d + ':' + sec_d;
+
 }
 
 
@@ -172,4 +182,4 @@ progressContainer.addEventListener('click', setProgress);
 audio.addEventListener('ended', nextSong);
 
 
-audio.addEventListener('timeupdate',DurTime);
+audio.addEventListener('timeupdate', DurTime);
